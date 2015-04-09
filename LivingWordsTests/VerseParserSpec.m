98d5b7c5334e71bfb2f130e94460b901151af836
Teddy Ku <teddypku@gmail.com>
2015-04-09 10:54:05 -0400

    verse parser spec improved

--- LivingWordsTests/VerseParserSpec.m
+++ LivingWordsTests/VerseParserSpec.m
@@ -12,9 +12,9 @@ describe(@"VerseParser", ^{
             NSOrderedSet *set = [VerseParser parseString:verseString];
             ParsedVerse *result = [set firstObject];
 
-            [[expectFutureValue(result.book) shouldEventually] equal:@"John"];
-            [[expectFutureValue(result.chapterStart) shouldEventually] equal:@3];
-            [[expectFutureValue(result.numberStart) shouldEventually] equal:@16];
+            [[result.book should] equal:@"John"];
+            [[result.chapterStart should] equal:@3];
+            [[result.numberStart should] equal:@16];
         });
 
         it(@"returns verse for numbered book", ^{
@@ -23,9 +23,9 @@ describe(@"VerseParser", ^{
             NSOrderedSet *set = [VerseParser parseString:verseString];
             ParsedVerse *result = [set firstObject];
 
-            [[expectFutureValue(result.book) shouldEventually] equal:@"1 John"];
-            [[expectFutureValue(result.chapterStart) shouldEventually] equal:@3];
-            [[expectFutureValue(result.numberStart) shouldEventually] equal:@16];
+            [[result.book should] equal:@"1 John"];
+            [[result.chapterStart should] equal:@3];
+            [[result.numberStart should] equal:@16];
         });
 
         it(@"returns verse for range of numbers", ^{
@@ -34,10 +34,10 @@ describe(@"VerseParser", ^{
             NSOrderedSet *set = [VerseParser parseString:verseString];
             ParsedVerse *result = [set firstObject];
 
-            [[expectFutureValue(result.book) shouldEventually] equal:@"1 John"];
-            [[expectFutureValue(result.chapterStart) shouldEventually] equal:@3];
-            [[expectFutureValue(result.numberStart) shouldEventually] equal:@16];
-            [[expectFutureValue(result.numberEnd) shouldEventually] equal:@17];
+            [[result.book should] equal:@"1 John"];
+            [[result.chapterStart should] equal:@3];
+            [[result.numberStart should] equal:@16];
+            [[result.numberEnd should] equal:@17];
         });
 
         it(@"returns multiple verses", ^{
@@ -47,14 +47,14 @@ describe(@"VerseParser", ^{
             ParsedVerse *resultOne = [set firstObject];
             ParsedVerse *resultTwo = [set lastObject];
 
-            [[expectFutureValue(resultOne.book) shouldEventually] equal:@"1 John"];
-            [[expectFutureValue(resultOne.chapterStart) shouldEventually] equal:@3];
-            [[expectFutureValue(resultOne.numberStart) shouldEventually] equal:@16];
-            [[expectFutureValue(resultOne.numberEnd) shouldEventually] equal:@17];
+            [[resultOne.book should] equal:@"1 John"];
+            [[resultOne.chapterStart should] equal:@3];
+            [[resultOne.numberStart should] equal:@16];
+            [[resultOne.numberEnd should] equal:@17];
 
-            [[expectFutureValue(resultTwo.book) shouldEventually] equal:@"John"];
-            [[expectFutureValue(resultTwo.chapterStart) shouldEventually] equal:@3];
-            [[expectFutureValue(resultTwo.numberStart) shouldEventually] equal:@16];
+            [[resultTwo.book should] equal:@"John"];
+            [[resultTwo.chapterStart should] equal:@3];
+            [[resultTwo.numberStart should] equal:@16];
         });
     });
 
