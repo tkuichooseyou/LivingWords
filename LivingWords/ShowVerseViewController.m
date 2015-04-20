81ed800566a732cdc02682b645f172515f8bd64c
Teddy Ku <teddypku@gmail.com>
2015-04-20 12:17:08 -0400

    added esv epub; showing verse using webview with table of contents

--- LivingWords/ShowVerseViewController.m
+++ LivingWords/ShowVerseViewController.m
@@ -1,9 +1,66 @@
+#import <KFEpubKit/KFEpubKit.h>
 #import "ShowVerseViewController.h"
+#import "ParsedVerse.h"
+
+@interface ShowVerseViewController () <KFEpubControllerDelegate>
+
+@property (nonatomic, strong) KFEpubController *epubController;
+@property (nonatomic, strong) KFEpubContentModel *contentModel;
+@property (nonatomic) NSUInteger spineIndex;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) ParsedVerse *parsedVerse;
+
+@end
 
 @implementation ShowVerseViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSURL *epubURL = [[NSBundle mainBundle] URLForResource:@"esv_classic_reference_bible" withExtension:@"epub"];
+    NSURL *documentsURL = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    self.epubController = [[KFEpubController alloc] initWithEpubURL:epubURL andDestinationFolder:documentsURL];
+    self.epubController.delegate = self;
+    [self.epubController openAsynchronous:YES];
+}
 
 - (IBAction)doneTapped:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+#pragma mark Epub Contents
+
+- (void)updateContentForSpineIndex:(NSUInteger)currentSpineIndex
+{
+    NSString *contentFile = self.contentModel.manifest[self.contentModel.spine[currentSpineIndex]][@"href"];
+    NSURL *contentURL = [self.epubController.epubContentBaseURL URLByAppendingPathComponent:contentFile];
+    NSLog(@"content URL :%@", contentURL);
+
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:contentURL];
+    [self.webView loadRequest:request];
+}
+
+#pragma mark KFEpubControllerDelegate Methods
+
+- (void)epubController:(KFEpubController *)controller willOpenEpub:(NSURL *)epubURL
+{
+    NSLog(@"will open epub");
+}
+
+
+- (void)epubController:(KFEpubController *)controller didOpenEpub:(KFEpubContentModel *)contentModel
+{
+    NSLog(@"opened: %@", contentModel.metaData[@"title"]);
+    self.contentModel = contentModel;
+    self.spineIndex = 4;
+    [self updateContentForSpineIndex:self.spineIndex];
+}
+
+
+- (void)epubController:(KFEpubController *)controller didFailWithError:(NSError *)error
+{
+    NSLog(@"epubController:didFailWithError: %@", error.description);
+}
+
 
 @end
