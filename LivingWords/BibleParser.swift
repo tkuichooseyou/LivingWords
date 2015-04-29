597e9648c0358bcb23a4287ce5ef0e2b0988ed14
Teddy Ku <teddypku@gmail.com>
2015-04-29 16:06:20 -0400

    verse ranges work

--- LivingWords/BibleParser.swift
+++ LivingWords/BibleParser.swift
@@ -3,26 +3,43 @@ import UIKit
 public class BibleParser: NSObject {
     public class func textForParsedVerse(parsedVerse:ParsedVerse) -> String {
         var error: NSError?
-        let chapterStartPattern = "\"chapter-num\">\\s\(parsedVerse.chapterStart)\\s</span>"
-        let verseNumberStartPattern = "\"verse-num\">\(parsedVerse.numberStart)</span>(?:\\s?<span>)?(.*?)<"
-        let pattern = parsedVerse.numberStart == 1 ?
-            chapterStartPattern + "(.*?)<" :
-            chapterStartPattern + "(?:.*?)" + verseNumberStartPattern
+        let verseStart = parsedVerse.numberStart.integerValue
+        let verseEnd = parsedVerse.numberEnd.integerValue
+        let verseRange = (verseStart...verseEnd)
+        var patterns: [String] = []
+
+        for verseNum in verseRange {
+            let verseNumberStartPattern = "\"verse-num\">\(verseNum)</span>(?:\\s?<span>)?(.*?)<"
+            let chapterStartPattern = "\"chapter-num\">\\s\(parsedVerse.chapterStart)\\s</span>"
+            if verseNum == 1 {
+                patterns.append(chapterStartPattern + "(.*?)<")
+            } else {
+                patterns.append(chapterStartPattern + "(?:.*?)" + verseNumberStartPattern)
+            }
+        }
 
         if let bookPath = NSBundle.mainBundle().pathForResource(parsedVerse.book.lowercaseString, ofType: "html", inDirectory: "esv"),
-            let bookString = NSString(contentsOfFile: bookPath, encoding: NSUTF8StringEncoding, error: &error) as? String,
-            let match = matchesForRegexInText(pattern, text:bookString).first {
-                return match.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+            bookString = NSString(contentsOfFile: bookPath, encoding: NSUTF8StringEncoding, error: &error) as? String {
+                return patterns
+                    .map { self.matchesForRegexInText($0, text:bookString).first }
+                    .reduce("", combine: {
+                        if let y = $1 {return $0 + " " + y}
+                        return $0
+                    })
+                    .stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        } else {
+            println(error)
         }
 
         return ""
     }
 
     class func matchesForRegexInText(regex: String!, text: String!) -> [String] {
+        var error: NSError?
         let regex = NSRegularExpression(
             pattern: regex,
             options: NSRegularExpressionOptions.DotMatchesLineSeparators,
-            error: nil)!
+            error: &error)!
         let nsString = text as NSString
         let results = regex.matchesInString(text,
             options: nil, range: NSMakeRange(0, nsString.length))
--- LivingWordsTests/BibleParserSpec.swift
+++ LivingWordsTests/BibleParserSpec.swift
@@ -10,6 +10,8 @@ class BibleParserSpec: QuickSpec {
                     parsedVerse.book = "John"
                     parsedVerse.chapterStart = 3
                     parsedVerse.numberStart = 16
+                    parsedVerse.chapterEnd = 3
+                    parsedVerse.numberEnd = 16
                     let expected = "\"For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life."
 
                     let result = BibleParser.textForParsedVerse(parsedVerse)
@@ -22,6 +24,8 @@ class BibleParserSpec: QuickSpec {
                     parsedVerse.book = "Genesis"
                     parsedVerse.chapterStart = 1
                     parsedVerse.numberStart = 5
+                    parsedVerse.chapterEnd = 1
+                    parsedVerse.numberEnd = 5
                     let expected = "God called the light Day, and the darkness he called Night. And there was evening and there was morning, the first day."
 
                     let result = BibleParser.textForParsedVerse(parsedVerse)
@@ -34,7 +38,23 @@ class BibleParserSpec: QuickSpec {
                     parsedVerse.book = "Genesis"
                     parsedVerse.chapterStart = 21
                     parsedVerse.numberStart = 1
+                    parsedVerse.chapterEnd = 21
+                    parsedVerse.numberEnd = 1
                     let expected = "The LORD visited Sarah as he had said, and the LORD did to Sarah as he had promised."
+
+                    let result = BibleParser.textForParsedVerse(parsedVerse)
+
+                    expect(result) == expected
+                }
+
+                it("returns text for range in john 3:16-18 verse") {
+                    let parsedVerse = ParsedVerse()
+                    parsedVerse.book = "John"
+                    parsedVerse.chapterStart = 3
+                    parsedVerse.numberStart = 16
+                    parsedVerse.chapterEnd = 3
+                    parsedVerse.numberEnd = 18
+                    let expected = "\"For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life. For God did not send his Son into the world to condemn the world, but in order that the world might be saved through him. Whoever believes in him is not condemned, but whoever does not believe is condemned already, because he has not believed in the name of the only Son of God."
 
                     let result = BibleParser.textForParsedVerse(parsedVerse)
 
